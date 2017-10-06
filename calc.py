d0cbb8a253db4d5e00589cc98fd71fa87d6c6a13
gs511 <gurmukh@xyz.com>
2017-10-06 18:07:30 +0530

    calculator

---
+++ calc.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template,request
+
+app= Flask(__name__)
+
+@app.route("/")
+def main():
+	return "welcome to calculator. type the operation you want to perform after/ eg: /add/number"
+
+@app.route("/<int:number>")
+def numb(number):
+	return "square of your number is %i" %number^3
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
+	
